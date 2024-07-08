1344416c8961dbe665eccd2a37259117729102d0
rafiframa <rafiframa@gmail.com>
2024-07-08 19:19:46 +0700

    chore:migration

---
+++ server/seeders/20240708112237-channel-views.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    let clipJson = require(`../db/Clippertrack.json`)
+    let clips = clipJson
+    .filter(e => e !== null && e !== undefined && e.channelName !== null && e.channelName !== undefined)
+    .map(e => ({
+      ...e,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }));
+
+    return queryInterface.bulkInsert('ChannelViews', clips);
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
