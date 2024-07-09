21cb34089f5c5df83bccceb2b5e5851bf58803fc
gerfrimrp <alans.gefri@gmail.com>
2024-07-09 23:53:36 +0700

    chore: seeding from API

--- seeders/20240709041809-character-seed.js
+++ seeders/20240709041809-character-seed.js
@@ -5,9 +5,28 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     const axios = require('axios');
 
+    const options = {
+      method: `GET`,
+      url: `https://gsi.fly.dev/characters/?page=1&limit=51`
+    };
+    try {
+      const response = await axios.request(options)
+      response.data.results.map((e) => {
+        e.createdAt = e.updatedAt = new Date();
+        return e
+      })
+      await queryInterface.bulkInsert('Characters', response.data.results)
+      // console.log(response.data.results)
+    } catch (err) {
+      console.error(err)
+    }
   },
 
   async down(queryInterface, Sequelize) {
-
+    await queryInterface.bulkDelete("Characters", null, {
+      truncate: true,
+      restartIdentity: true,
+      cascade: true
+    });
   }
 };
