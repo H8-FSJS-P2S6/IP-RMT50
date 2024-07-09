a6d00bbf46105be146f50fb1b66fe70077705800
rafiframa <rafiframa@gmail.com>
2024-07-09 22:35:39 +0700

    client: added login

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Homepage from './Login.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <>
+    <Homepage />
+    </>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  
+  <RouterProvider router={router} />
 )
