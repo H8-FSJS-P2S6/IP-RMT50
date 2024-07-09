65f0d2c97ca984de3dfa748b464712732ba67674
Revi28 <revirifaldi@gmail.com>
2024-07-10 01:44:24 +0700

    feat: menampilkan semua data api external dalam bentuk table dengan metode pageination

--- server/app.js
+++ server/app.js
@@ -5,13 +5,13 @@ if(process.env.NODE_ENV !== "production"){
 }
 
 
-
+const cors = require('cors')
 const express = require("express");
 const app = express();
 const routes = require("./routes");
 const errorHandler = require("./middlewares/errorHandler");
 // const port = process.env.port || 3000
-
+app.use(cors())
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(routes);
