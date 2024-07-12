9142095b4b294e09175b0e7fe905e99449a41134
gerfrimrp <alans.gefri@gmail.com>
2024-07-12 11:53:12 +0700

    fix: association

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      User.hasOne(models.Party)
+      User.hasOne(models.Party, { foreignKey: `UserId` })
     }
   }
   User.init({
